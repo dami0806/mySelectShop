1aaddb6dc3266ae1e5552995773a35e61000dd9f
dami0806 <dami0804@naver.com>
2024-06-12 15:36:34 +0900

    관심상품 조회 API

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -7,6 +7,8 @@ import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -20,9 +22,16 @@ public class ProductController {
         return productService.createProduct(requestDto);
     }
 
+    // 상품 희망가격 넣기
     @PutMapping("/products/{id}")
     public ProductResponseDto updateproduct(@PathVariable long id, @RequestBody ProductMypriceRequestDto requestDto) {
         return productService.updateProduct(id, requestDto);
+    }
+
+    // 관심상품 조회하기
+    @GetMapping("/products")
+    public List<ProductResponseDto> getProducts() {
+        return productService.getProducts();
     }
 
 }
